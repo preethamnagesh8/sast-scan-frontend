2b1a1f8b49ca37021263a8042fbd6a927266a174
Preetham Nagesh <preethamnagesh8@gmail.com>
2021-04-13 17:30:35 +0530

    Added bootstrap 5 and sample header and footer

---
+++ sast-scan-frontend/src/app/landing-hero/landing-hero.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-landing-hero',
+  templateUrl: './landing-hero.component.html',
+  styleUrls: ['./landing-hero.component.css']
+})
+export class LandingHeroComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
